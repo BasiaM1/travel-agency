ad33ad678b6b025037cfd883c5f3404b8bc7f979
BasiaM1 <b.murzynowska@wp.pl>
2020-10-25 15:38:52 +0100

    Add formatTime and tests for time

---
+++ src/utils/formatTime.js
@@ -0,0 +1,18 @@
+export const formatTime = (param) => {
+  const seconds = Math.floor(param % 60);
+  const minutes = Math.floor((param / 60) % 60);
+  const hours = Math.floor(param / 3600);
+
+  const time =
+    hours.toString().padStart(2, '0') +
+    ':' +
+    minutes.toString().padStart(2, '0') +
+    ':' +
+    seconds.toString().padStart(2, '0');
+
+  if (param === undefined || typeof (param != 'number') || param < 0) {
+    return null;
+  } else {
+    return time;
+  }
+};
